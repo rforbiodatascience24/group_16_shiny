9808f5dade291b616210793bbd2423d19f290a73
KonstantinaGkopi <s243692@dtu.dk>
2024-11-06 14:27:02 +0000

    added aa freq plot, base count table, comments and about text by s243692

--- ui.R
+++ ui.R
@@ -1,27 +1,35 @@
 # Define the User Interface (Frontend)
 ui <- page_fluid(
+  # App title section
   layout_columns(
     col_widths = 12,
     card(
       titlePanel("Virtual Central Dogma"),
       style = "background-color: #f0f0f0; padding: 15px;"
     )),
+  
+  # About section with app description
   layout_columns(
     col_widths = 12,
     card(
       titlePanel("About"),
-      helpText("This app simulates the central dogma of molecular biology.")
+      helpText("This app demonstrates the central dogma of molecular biology by simulating DNA transcription and translation. Users can generate a custom DNA sequence, transcribe it to RNA, and translate it into a protein sequence. Explore base composition, visualize amino acid frequencies, and gain insights into genetic information processing.")
     )),
+  
+  # Input section for generating DNA
   layout_columns(
     col_widths = 12,
     card(
       card_header("Virtual Gene Generator"),
+      # Slider for number of bases
       sliderInput(inputId = "n_bases",
                   label = "Number of bases:",
                   min = 1,
                   max = 60,
                   value = 30,
                   width = "100%"),
+      
+      # Numeric inputs for base probabilities
       layout_columns(
         col_widths = c(3, 3, 3, 3),
         numericInput(inputId = "prob_A",
@@ -49,6 +57,8 @@ ui <- page_fluid(
                      max = 1,
                      step = 0.05)
       ))),
+  
+  # Output section displaying DNA, RNA, Protein, and analysis results
   layout_columns(
     col_widths = 12,
     card(
@@ -58,6 +68,12 @@ ui <- page_fluid(
         tags$div(
           tags$h4("DNA"),
           verbatimTextOutput(outputId = "dna")
+        ),
+        
+        # Base Count Table Output
+        tags$div(
+          tags$h4("Base Count"),
+          tableOutput(outputId = "baseCount")
         ),
         
         # RNA Output
@@ -70,6 +86,12 @@ ui <- page_fluid(
         tags$div(
           tags$h4("Protein"),
           verbatimTextOutput(outputId = "protein")
+        ),
+        
+        # Amino Acid Frequency Plot
+        tags$div(
+          tags$h4("Amino Acid Frequency Plot"),
+          plotOutput(outputId = "aaPlot")
         )
       )
     ))
